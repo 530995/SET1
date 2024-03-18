b60902843ebbd01827c25f78b8cc4a510fe834e4
Lorenzo Davoli <530995@student.saxion.nl>
2024-03-18 15:58:11 +0100

    third commit

---
+++ multiplication.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lorenzo on 18/03/2024.
+//
+
+#include "multiplication.h"
---
+++ multiplication.h
@@ -0,0 +1,14 @@
+//
+// Created by Lorenzo on 18/03/2024.
+//
+
+#ifndef WEEK_1_MULTIPLICATION_H
+#define WEEK_1_MULTIPLICATION_H
+
+
+class multiplication {
+
+};
+
+
+#endif //WEEK_1_MULTIPLICATION_H
