7f916fe0a2fc9c7d84431890c8a51befccbad271
Lorenzo Davoli <530995@student.saxion.nl>
2024-03-20 16:29:28 +0100

    subtraction

---
+++ subtraction.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lorenzo on 20/03/2024.
+//
+
+#include "subtraction.h"
---
+++ subtraction.h
@@ -0,0 +1,14 @@
+//
+// Created by Lorenzo on 20/03/2024.
+//
+
+#ifndef WEEK_1_SUBTRACTION_H
+#define WEEK_1_SUBTRACTION_H
+
+
+class subtraction {
+
+};
+
+
+#endif //WEEK_1_SUBTRACTION_H
